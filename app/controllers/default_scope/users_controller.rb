e25424e154ac0347172f1deab7e283d50901c7ec
Nikodem <ntokarsk@student.agh.edu.pl>
2017-01-07 19:42:54 +0100

    scopes created

---
+++ app/controllers/default_scope/users_controller.rb
@@ -0,0 +1,28 @@
+module DefaultScope 
+	class UsersController < ApplicationController
+		def show
+			@user = User.find(params[:id])
+            render "default_scope/users/show"
+		end
+
+		def new
+			@user = User.new
+		end
+
+		def create
+    	@user = User.new(user_params)
+    	if @user.save
+        flash[:success] = "Welcome to the Sample App!"
+      	  redirect_to user_url(@user)
+    	else
+      		render 'new'
+    	end
+  	end
+
+  	private
+	    def user_params
+	      params.require(:user).permit(:name, :email, :password,
+	                                   :password_confirmation)
+	    end
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   #get  'users/new'
-  get  '/signup',      to:'users#new'
-  root 'static_pages#home'
-  get  '/help',    to: 'static_pages#help'
-  get  '/about',   to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
-  resources :users
+	scope module: :default_scope do
+	  get  '/signup',      to:'users#new'
+	  root 'static_pages#home'
+	  get  '/help',        to: 'static_pages#help'
+	  get  '/about',       to: 'static_pages#about'
+	  get  '/contact',     to: 'static_pages#contact'
+	  resources :users
+	end
 end
